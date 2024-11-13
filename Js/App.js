6c81a1c21f57db52ba1b43bc021e3bc653f575a7
Ahmetdlpr <mybqadresadrs@outlook.com>
2024-11-13 15:38:34 +0300

    App updated

--- Js/App.js
+++ Js/App.js
@@ -25,6 +25,16 @@ const searchdata=[
         link1:'https://www.reddit.com/r/ZargoryanGalaksisi/',
         link2:'https://www.reddit.com/r/Turkey/',
 
+    },
+    {
+        searchtitle:"Youtube",
+        link1:'https://www.youtube.com/@NoluyoYaa',
+        link2:'https://www.youtube.com/@IShowSpeed',
+        link3:'https://www.youtube.com/c/%C3%A7okg%C3%BCzelhareketler',
+        link4:'https://www.youtube.com/@HTalksYoutube',
+        link5:'https://www.youtube.com/@trthaber',
+
+
     }
 ]
 
@@ -168,6 +178,103 @@ const Search=()=>{
             }
 
         
+            if(Searchinput.value=="Youtube")
+                {
+                    
+                    Searchinput.value="";
+                  
+                    inputform.remove();
+                
+                    Appcontainer.appendChild(searchcontainer);
+                
+                  for(let i=0; i<searchdata.length; i++)
+                  {
+                         if(searchdata[i].searchtitle=="Youtube")
+                         {
+                            Pageetitle.textContent="FavoritesExplorer/"+searchdata[i].searchtitle; 
+                            Searchtitle.textContent=searchdata[i].searchtitle;
+                             
+        
+                               /* Youtube link1  */
+                                 let li1=document.createElement("li");
+                                 let a1=document.createElement("a");
+                                 a1.classList.add("Searchlistitem");
+                                 a1.textContent="Noluyo ya";
+                                 a1.href=searchdata[i].link1;
+                                 a1.style.textDecoration="none";
+                                 a1.style.color="white";
+                
+                
+                               /* Youtube link2 */
+                            
+                                 let li2=document.createElement("li");
+                                 let a2=document.createElement("a");
+                                 a2.classList.add("Searchlistitem");
+                                 a2.textContent="IShowSpeed";
+                                 a2.href=searchdata[i].link2;
+                                 a2.style.textDecoration="none";
+                                 a2.style.color="white";
+                
+        
+                                  /* Youtube link3 */
+                                  let li3=document.createElement("li");
+                                  let a3=document.createElement("a");
+                                  a3.classList.add("Searchlistitem");
+                                  a3.textContent="Çok güzel hareketler bunlar";
+                                  a3.href=searchdata[i].link3;
+                                  a3.style.textDecoration="none";
+                                  a3.style.color="white";
+        
+        
+                                 /* Youtube link4 */
+                                 let li4=document.createElement("li");
+                                 let a4=document.createElement("a");
+                                 a4.classList.add("Searchlistitem");
+                                 a4.textContent="Htalks";
+                                 a4.href=searchdata[i].link4;
+                                 a4.style.textDecoration="none";
+                                 a4.style.color="white";
+        
+
+                                 
+                                 /* Youtube link5 */
+                                 let li5=document.createElement("li");
+                                 let a5=document.createElement("a");
+                                 a5.classList.add("Searchlistitem");
+                                 a5.textContent="Trt haber";
+                                 a5.href=searchdata[i].link5;
+                                 a5.style.textDecoration="none";
+                                 a5.style.color="white";
+
+
+        
+                                 /* Youtube link1 add */ 
+                                 li1.appendChild(a1);
+                                 List.appendChild(li1);
+                                /*  Youtube link2 add */ 
+                                 li2.appendChild(a2);
+                                 List.appendChild(li2);
+                                /* Youtube link3  add */ 
+                                 li3.appendChild(a3);
+                                 List.appendChild(li3);
+                                /* Youtube link4  add  */
+                                 li4.appendChild(a4);
+                                 List.appendChild(li4);
+
+                                 /* Youtube link5 */ 
+                                 li5.appendChild(a5);
+                                 List.appendChild(li5);
+        
+                                }
+                         
+                  }
+                }
+                
+        
+        
+        
+
+                
         const Psearch=()=>{
             searchcontainer.remove();
             Appcontainer.appendChild(inputform);
